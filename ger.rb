0811990afc57f76c0d1380458394d53ff15827d8
Randel Jiao <randeljiao@gmail.com>
2019-10-24 04:53:01 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -2,13 +2,21 @@ class Dog
   
   @@dog_names = []
   
-  def initialize(name)
-    @name = name 
+  def save
     @@dog_names << self
+  end
+  
+  def initialize(name)
+    @name = name
+    save
   end
   
   def self.all 
     @dog_names
+  end
+  
+  def self.print_all
+    @@dog_names.each {|name| puts name}
   end
   
 end
